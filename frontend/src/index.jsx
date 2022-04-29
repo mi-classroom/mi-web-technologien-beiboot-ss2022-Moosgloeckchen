272b9a24c039b95960bb289393f6ca6f2d31d5bc
Linnéa Do <73938303+Moosgloeckchen@users.noreply.github.com>
2022-04-29 21:37:18 +0200

    :heavy_plus_sign: add react

---
+++ frontend/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './assets/styles/index.css';
+
+ReactDOM.render(
+    <div>
+        <p>lala</p>
+    </div>,
+    document.getElementById('root'),
+);
