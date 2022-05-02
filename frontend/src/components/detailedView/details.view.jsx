cfb697a984f66d6bf977286250db6f760969df21
Linnéa Do <73938303+Moosgloeckchen@users.noreply.github.com>
2022-05-02 23:45:47 +0200

    :lipstick: add scss imports and style details view

--- frontend/src/components/detailedView/details.view.jsx
+++ frontend/src/components/detailedView/details.view.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { getFromLocalStorage } from '../../helpers/localStorage.helper';
+import './details.view.css';
 
-const DetailsView = () => {
+const DetailsView = ({bestOf, setisDetails}) => {
 
     const paintingNumber = getFromLocalStorage('paintingNumber');
 
+    const paintingArray = bestOf.filter((painting) => {
+        return (painting.sortingNumber === paintingNumber);
+    })
+
+    const painting = paintingArray[0];
     return (
-        <div>
-            <p>test</p>
+        <div className='DetailedView'>
+            <button className='Detaileview-backButton u-marginBs' onClick={() => setisDetails(false)}>Zurück</button>
+            <img className='DetailView-image u-marginBs' src={painting.images.overall.images[0].sizes.medium.src}/>
+            <span className='u-marginBs'>Titel: {painting.metadata.title}</span>
+            <span className='u-marginBs'>Datum: {painting.images.overall.images[0].metadata.date} </span>
+            <span>Besitzer: {painting.repository}</span>
         </div>
     )
 }
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './assets/styles/index.css';
+import './assets/styles/index.scss';
 
 ReactDOM.render(
     <div>
