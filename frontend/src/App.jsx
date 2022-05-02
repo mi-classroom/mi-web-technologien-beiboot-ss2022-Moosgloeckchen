ca6ef6c74149d5e17e8892b9675f2fb742d79cf5
Linnéa Do <73938303+Moosgloeckchen@users.noreply.github.com>
2022-05-02 23:14:39 +0200

    :art: format code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Overview from './components/overview/overview';
 
 import Data from "./../../data/cda-paintings-2022-04-22.de.json";
@@ -8,14 +8,32 @@ import DetailsView from './components/detailedView/details.view';
 const App = () => {
 
     const [isDetails, setisDetails] = useState(false)
+    const [bestOf, setpaintingsBestOf] = useState(null)
+
+    const masterpieces = Data.items;
+
+    useEffect(() => {
+      async function getData() {
+        const piecesBestOf = masterpieces.filter((painting) => {
+            return painting.isBestOf;
+        })
+        setpaintingsBestOf(piecesBestOf);
+      }
+
+      getData();
+  
+    return () => {
+    }
+  }, [])
+
 
     return (
         <div>
             {!isDetails 
             ? 
-                <Overview setisDetails={setisDetails} />
+                <Overview bestOf={bestOf} setisDetails={setisDetails} />
             :
-                <DetailsView paintings />
+                <DetailsView bestOf={bestOf} setisDetails={setisDetails}/>
             }
         </div>
     )
--- frontend/src/components/overview/overview.jsx
+++ frontend/src/components/overview/overview.jsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import { saveInLocalStorage } from '../../helpers/localStorage.helper';
 import { mergeSort } from '../../helpers/sorting.helper';
-import Data from "./../../../../data/cda-paintings-2022-04-22.de.json";
 import './overview.css';
 
-const Overview = ({setisDetails}) => {
+const Overview = ({bestOf, setisDetails}) => {
+    const paintings = bestOf ? bestOf : [];
 
-    const masterpieces = Data.items;
-
-    const bestOf = masterpieces.filter((painting) => {
-        return painting.isBestOf;
-    })
-
-    mergeSort(bestOf);
+    mergeSort(paintings);
 
     const handleClick = (paintingNumber) => {
         setisDetails(true);
         saveInLocalStorage('paintingNumber', paintingNumber)
     }
 
-    const getPaintingsPreview = bestOf.map((painting, i) => (
+    const getPaintingsPreview = paintings.map((painting, i) => (
         <div key={i} className='Preview-painting'>
             <button onClick={() => handleClick(painting.sortingNumber)}>
                 <img src={painting.images.overall.images[0].sizes.medium.src} height='200px'/>
