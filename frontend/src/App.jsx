6e247df106fc8f22d8a8cc97a6578b5b89d85803
Linnéa Do <73938303+Moosgloeckchen@users.noreply.github.com>
2022-05-24 14:26:30 +0200

    :sparkles: add docker files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Overview from './components/overview/overview';
 
-import Data from "./../../data/cda-paintings-2022-04-22.de.json";
+import Data from "./../data/cda-paintings-2022-04-22.de.json";
 import { mergeSort } from './helpers/sorting.helper';
 import DetailsView from './components/detailedView/details.view';
 
