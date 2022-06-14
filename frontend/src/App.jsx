32540bf0b240376efeac16c8890121feed055d4e
Linnéa Do <73938303+Moosgloeckchen@users.noreply.github.com>
2022-06-14 21:16:14 +0200

    :sparkles: display paintings on plane

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import Data from "./../data/cda-paintings-2022-04-22.de.json";
 
 import * as THREE from 'three'
 import { Canvas, extend, useFrame, useThree } from '@react-three/fiber';
-import { softShadows, BakeShadows, RoundedBox, Environment, TransformControls, useCursor, MeshReflectorMaterial, Image, Text, CubeCamera, useGLTF, useBoxProjectedEnv } from '@react-three/drei'
+import { softShadows, BakeShadows, RoundedBox, Environment, TransformControls, MeshReflectorMaterial, Image, Text, CubeCamera, useGLTF, useBoxProjectedEnv } from '@react-three/drei'
 import { useRoute, useLocation } from 'wouter'
 import CameraControls from 'camera-controls'
 import getUuid from 'uuid-by-string'
@@ -11,7 +11,7 @@ import { mergeSort } from './helpers/sorting.helper';
 
 const GOLDENRATIO = 1.61803398875
 
-const App = ({images}) => {
+const App = () => {
 
   const [isDetails, setisDetails] = useState(false)
   const [bestOf, setpaintingsBestOf] = useState(null)
@@ -36,18 +36,13 @@ const App = ({images}) => {
 
   mergeSort(paintings);
 
-  const getPaintingsPreview = paintings.map((painting, i) =>  {
-      const maxDimensions = painting.images.overall.infos.maxDimensions;
-      const fixWidth = 70;
-      const paintingWidth = maxDimensions.width;
-      const paintingHeight = maxDimensions.height;
-      const ratio = (paintingHeight / paintingWidth)
-      const fixHeight = ratio * fixWidth;
-
-      return (
-        <Frames key={i} images={painting.images.overall.images[0].sizes.medium.src} />
-      )
-  })
+  const groupPaintings = (paintings) =>
+    paintings.reduce((groups, painting) => {
+      const group = groups[painting.sortingInfo.year] || [];
+      group.push(painting);
+      groups[painting.sortingInfo.year] = group;
+      return groups;
+    }, {});
 
   return (
     <Canvas shadows camera={{ position: [20, 15, 50], fov: (42, 0.05) }}>
@@ -56,10 +51,11 @@ const App = ({images}) => {
       <Suspense fallback={null}>
         <group position={[0, -0.9, -3]}>
           {/* <Plane color="hotpink" rotation-x={-Math.PI / 2} position-z={2} scale={[4, 40, 0.2]} /> */}
-          <Frames images={images} />
-          
+          {Object.entries(groupPaintings(paintings)).map(([year, group, i]) => (
+            <Frames key={i} paintings={paintings} group={group}/>
+          ))}
           <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
-            <planeGeometry args={[50, 50]} />
+            <planeGeometry args={[40, 100]} />
             <MeshReflectorMaterial
               blur={[300, 100]}
               resolution={2048}
@@ -76,13 +72,10 @@ const App = ({images}) => {
         </group>
         <Controls />
         <Zoom />
-        <Environment preset="city" />
-        <BakeShadows />
       </Suspense>
     </Canvas>
   )
 }
-
 
 function Zoom({ vec = new THREE.Vector3(0, 0, 100) }) {
   return useFrame((state) => {
@@ -104,69 +97,56 @@ function Controls() {
   return <cameraControls ref={ref} args={[camera, gl.domElement]} />
 }
 
-function Frames({ images, q = new THREE.Quaternion(), p = new THREE.Vector3() }) {
-  const ref = useRef()
-  const clicked = useRef()
-  const [, params] = useRoute('/item/:id')
-  const [, setLocation] = useLocation()
-  useEffect(() => {
-    clicked.current = ref.current.getObjectByName(params?.id)
-    if (clicked.current) {
-      clicked.current.parent.updateWorldMatrix(true, true)
-      clicked.current.parent.localToWorld(p.set(0, GOLDENRATIO / 2, 1.25))
-      clicked.current.parent.getWorldQuaternion(q)
-    } else {
-      p.set(0, 0, 5.5)
-      q.identity()
-    }
+function Frames({ paintings, group}) {
+  const getPaintings = group.map((painting, i) =>  {
+    const maxDimensions = painting.images.overall.infos.maxDimensions;
+    const paintingWidth = maxDimensions.width;
+    const paintingHeight = maxDimensions.height;
+    const ratio = (paintingHeight / paintingWidth)
+    const positionZ = painting.sortingInfo.year-1525;
+    const positionX = i * 2
+
+    const useProxy = (text) => {
+      const split = text.split('imageserver-2022/');
+      return 'https://lucascranach.org/data-proxy/image.php?subpath=/' + split[1];
+    };
+    
+    return (
+      <Frame paintings={paintings} key={i} index={i} url={useProxy(painting.images.overall.images[0].sizes.medium.src)} year={painting.sortingInfo.year} position={[positionX, 0, positionZ]} rotation= {[0, 0, 0]} ratio={ratio}/>
+    )
   })
-  useFrame((state, dt) => {
-    state.camera.position.lerp(p, 0.025)
-    state.camera.quaternion.slerp(q, 0.025)
-  })
-  
+
   return (
-    <group
-      ref={ref}
-      onClick={(e) => (e.stopPropagation(), setLocation(clicked.current === e.object ? '/' : '/item/' + e.object.name))}
-      onPointerMissed={() => setLocation('/')}>
-      {images.map((props) => <Frame key={props.url} {...props} /> /* prettier-ignore */)}
+    <group>
+      {getPaintings}
     </group>
   )
 }
 
 function Frame({ url, c = new THREE.Color(), ...props }) {
-  const [hovered, hover] = useState(false)
-  const [rnd] = useState(() => Math.random())
   const image = useRef()
   const frame = useRef()
   const name = getUuid(url)
-  useCursor(hovered)
-  useFrame((state) => {
-    image.current.material.zoom = 2 + Math.sin(rnd * 10000 + state.clock.elapsedTime / 3) / 2
-    image.current.scale.x = THREE.MathUtils.lerp(image.current.scale.x, 0.85 * (hovered ? 0.85 : 1), 0.1)
-    image.current.scale.y = THREE.MathUtils.lerp(image.current.scale.y, 0.9 * (hovered ? 0.905 : 1), 0.1)
-    frame.current.material.color.lerp(c.set(hovered ? 'orange' : 'white'), 0.1)
-  })
+
   return (
-    <group {...props}>
-      <mesh
-        name={name}
-        onPointerOver={(e) => (e.stopPropagation(), hover(true))}
-        onPointerOut={() => hover(false)}
-        scale={[1, GOLDENRATIO, 0.05]}
-        position={[0, GOLDENRATIO / 2, 0]}>
-        <boxGeometry />
-        <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
-        <mesh ref={frame} raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]}>
+    <group>
+      <group {...props}>
+        <mesh
+          name={name}
+          scale={[1, props.ratio, 0.05]}
+          position={[0, 1, 0]}>
           <boxGeometry />
-          <meshBasicMaterial toneMapped={false} fog={false} />
+          <meshStandardMaterial color="#151515" metalness={0.5} roughness={0.5} envMapIntensity={2} />
+          <mesh ref={frame} raycast={() => null} scale={[0.9, 0.93, 0.9]} position={[0, 0, 0.2]}>
+            <boxGeometry />
+            <meshBasicMaterial toneMapped={false} fog={false} />
+          </mesh>
+          <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
         </mesh>
-        <Image raycast={() => null} ref={image} position={[0, 0, 0.7]} url={url} />
-      </mesh>
-      <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, GOLDENRATIO, 0]} fontSize={0.025}>
-        {name.split('-').join(' ')}
-      </Text>
+        <Text maxWidth={0.1} anchorX="left" anchorY="top" position={[0.55, props.ratio, 0]} fontSize={0.025}>
+          {name.split('-').join(' ')}
+        </Text>
+      </group>
     </group>
   )
 }
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,25 +4,9 @@ import App from './App';
 
 import './assets/styles/index.scss';
 
-
-const pexel = (id) => `https://images.pexels.com/photos/${id}/pexels-photo-${id}.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260`
-const images2 = [
-  //positin: [x, y, z] -> rechts/links, oben/unten, vorne/hinten
-
-  { position: [0, 0, 0], rotation: [0, 0, 0], url: pexel(1103970) },
-  { position: [0, 0, 1], rotation: [0, 0, 0], url: pexel(416430) },
-  { position: [0, 0, 1.5], rotation: [0, 0, 0], url: pexel(310452) },
-  { position: [0, 0, 3], rotation: [0, 0, 0], url: pexel(327482) },
-  { position: [0, 0, 4], rotation: [0, 0, 0], url: pexel(325185) },
-  { position: [0, 0, 5.5], rotation: [0, 0, 0], url: pexel(358574) },
-  { position: [0, 0, 7], rotation: [0, 0, 0], url: pexel(227675) },
-  { position: [0, 0, 7.5], rotation: [0, 0, 0], url: pexel(911738) },
-  { position: [0, 0, 8], rotation: [0, 0, 0], url: pexel(1738986) }
-]
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App images={images2}/>
+    <App/>
   </React.StrictMode>
 )
 
