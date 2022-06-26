54643facdb9097862766f3b424b2616e372bac67
Linnéa Do <73938303+Moosgloeckchen@users.noreply.github.com>
2022-06-26 22:01:28 +0200

    :bug: fix image ratio and display

---
+++ frontend/src/components/frame/frame.jsx
@@ -0,0 +1,29 @@
+import { Image, Text } from "@react-three/drei";
+import React, { useRef } from "react";
+import * as THREE from 'three'
+
+export const Frame = ({ url, c = new THREE.Color(),  height, ...props }) => {
+  const image = useRef()
+  const frame = useRef()
+
+  const descriptionString = 'Titel: ' + props.title + '\n' + 'Künstler: ' + props.artist + '\n' + 'Datum: ' + props.date + '\n' + 'Besitzer: ' + props.owner;
+  const maxDimensions = props.maxDimensions;
+  const paintingHeight = (maxDimensions.height / 500) / 10;
+  const ratio = props.ratio;
+  const width = paintingHeight * ratio;
+  
+  return (
+    <group>
+      <group {...props}>
+        <mesh ref={frame} scale={[width, paintingHeight, 0.03]} position={[0, 2, 0.28]}>
+          <boxGeometry />
+          <meshBasicMaterial />
+        </mesh>
+        <Image ref={image} position={[0, 2, 0.3]} scale={[width, paintingHeight, 1]} url={url} />
+        <Text maxWidth={0.8} anchorX="center" anchorY="middle" position={[0, 1, 0.5]} fontSize={0.03} rotation={[5, 0, 0]}>
+          {descriptionString}
+        </Text>
+      </group>
+    </group>
+  )
+}
--- frontend/src/components/frames/frames.jsx
+++ frontend/src/components/frames/frames.jsx
@@ -1,24 +1,28 @@
-import { Text } from "@react-three/drei";
-import React, { useRef } from "react";
-import { Frame } from "../frame.jsx/frame";
+import React from "react";
+import { Frame } from "../frame/frame";
 import { Year } from "../year/year";
 
 export const Frames = ({ group }) => {
   const getPaintings = group.map((painting, i) => {
     const positionZ = painting.sortingInfo.year - 1525;
-    const positionX = i * 2
+    const positionX = i * 2.1;
 
     const useProxy = (text) => {
       const split = text.split('imageserver-2022/');
       return 'https://lucascranach.org/data-proxy/image.php?subpath=/' + split[1];
     };
 
+    const ratio = painting.images.overall.images[0].sizes.medium
+      ? painting.images.overall.images[0].sizes.medium.dimensions.width /
+          painting.images.overall.images[0].sizes.medium.dimensions.height
+      : 1
+
     return (
       <group>
         <Year position={[-2, 0, positionZ]} year={painting.sortingInfo.year} positionZ={positionZ} />
         <Frame
           key={i} index={i}
-          url={useProxy(painting.images.overall.images[0].sizes.medium.src)}
+          url={useProxy(painting.images.overall.images[0].sizes.medium?.src)}
           year={painting.sortingInfo.year}
           position={[positionX, 0, positionZ]}
           rotation={[0, 0, 0]}
@@ -27,6 +31,8 @@ export const Frames = ({ group }) => {
           artist={painting.involvedPersons[0].name}
           date={painting.images.overall.images[0].metadata.date}
           owner={painting.repository}
+          width={painting.images.overall.images[0].sizes.medium?.dimensions.width}
+          ratio={ratio}
         />
       </group>
     )
